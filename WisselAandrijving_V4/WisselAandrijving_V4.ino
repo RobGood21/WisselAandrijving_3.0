5bc244e05c7bdfb1d13815e941a5a2b39082622d
Rob Antonisse <robantonisse@gmail.com>
2023-11-26 16:27:39 +0100

    Start WA versie 4

---
+++ WisselAandrijving_V4/WisselAandrijving_V4.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		WisselAandrijving_V4.ino
+ Created:	11/26/2023 4:27:11 PM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
