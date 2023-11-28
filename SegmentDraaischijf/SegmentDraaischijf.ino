7832325dab561df2e35d28f8c3a32b2e1fefd33f
Rob Antonisse <robantonisse@gmail.com>
2023-11-28 11:40:47 +0100

    motor draait

--- SegmentDraaischijf/SegmentDraaischijf.ino
+++ SegmentDraaischijf/SegmentDraaischijf.ino
@@ -4,7 +4,7 @@
  Author:	Rob Antonisse
 
 
- Arduino Uno sketch voor de aansturing van een segment draaischijf met twee sporen, zoals op de 'Baan'. 
+ Arduino Uno sketch voor de aansturing van een segment draaischijf met twee sporen, zoals op de 'Baan'.
 
 
 */
@@ -16,12 +16,17 @@
 //constructors
 NmraDcc  Dcc;
 
+//constanten
+const int MinSpeed = 900;
 
 //variabelen
 unsigned long slowtimer = 0;
 byte readlast = 63; //B0011 1111
-
-
+unsigned int stepspeed = 0; //snelheid stappen in us. 
+unsigned long steptimer;
+byte stepfase = 0; //in fullstep max4 in halfstep max 8
+bool steprichting = false;
+bool stepdrive = false;
 
 void setup() {
 	Serial.begin(9600);
@@ -31,31 +36,40 @@ void setup() {
 	//Pinnen
 	DDRC = 0; //port C A0~A5 as inputs
 	PORTC = 63; //pullup weerstanden op pin A0~A5, tbv. switches en sensoren
-
+	DDRB = 15; //pinnen 8~11 als output
+	PORTB &= ~(15 << 0); //zet pinnen 8~11 laag
 
 }
 
 
-void loop() 
+void loop()
 {
 	Dcc.process();
+
 	if (millis() - slowtimer > 20) { //timer op 20ms
 		slowtimer = millis();
 		SW_exe(); //uitlezen switches en  sensoren.
+	}
 
+	if (stepdrive) {
+		if (micros() - steptimer > stepspeed+MinSpeed) { //900us is minimaal, hoogste haalbare snelheid van de 48byj-28
+			steptimer = micros();
+			Step_exe();
+		}
 	}
 
 }
 
-void SW_exe() 
+//drukknoppen en sensoren
+void SW_exe()
 {
 	//called from loop om de 20ms.
 	byte _read = PINC; //lees register C
 	_read &= ~(1 << 192); //clear bit7 en 6
-	byte  _change =_read ^ readlast; 
+	byte  _change = _read ^ readlast;
 	if (_change > 0) {
 		for (byte i = 0; i < 6; i++) {
-			if (_change & (1 << i)) { 
+			if (_change & (1 << i)) {
 				if (_read & (1 << i)) { //knop losgelaten
 					SWoff(i);
 				}
@@ -67,9 +81,73 @@ void SW_exe()
 	}
 	readlast = _read;
 }
-void SWon( byte _sw) {
+void SWon(byte _sw) {
 	Serial.print("knop aan: "); Serial.println(_sw);
+
+	switch (_sw) {
+	case 0:
+		stepdrive = !stepdrive;	
+		Stepoff();
+		break;
+	case 1:
+		steprichting = !steprichting;
+		break;
+	case 2:
+		break;
+
+	}
+
+
+
 }
-void SWoff( byte _sw) {
+void SWoff(byte _sw) {
 	Serial.print("knop uit: "); Serial.println(_sw);
+}
+//stappenmotor
+
+void Step_exe() {
+
+	//Serial.print(stepfase);
+
+	//stuurt de stappenmotor aan
+	Stepoff(); //zet alle spoelen uit
+
+	switch (stepfase) {
+	case 0:
+		PINB |= (1 << 0);
+		break;
+	case 1:
+		PINB |= (3 << 0);
+		break;
+	case 2:
+		PINB |= (1 << 1);
+		break;
+	case 3:
+		PINB |= (3 << 1);
+		break;
+	case 4:
+		PINB |= (1 << 2);
+		break;
+	case 5:
+		PINB |= (3 << 2);
+		break;
+	case 6:
+		PINB |= (1 << 3);
+		break;
+	case 7:
+		PINB |= (9 << 0);
+		break;
+	}
+
+	if (steprichting) {
+		stepfase--;
+		if (stepfase > 7)stepfase = 7;
+	}
+	else {
+		stepfase++;
+		if (stepfase > 7) stepfase = 0;
+	}
+}
+void Stepoff() {
+	PORTB &= ~(15 << 0);
 }
